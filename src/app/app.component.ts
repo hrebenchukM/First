c082d1440e37493ee36b634df70bad952988d321
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-05-08 02:19:41 +0300

    Component+Ex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';//Ипортировали хедер компонент
+import { LogoComponent } from './logo/logo.component';//Ипортировали  logo 
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+@Component({//декоратор компонент @ для AppComponent,динамически добавляем поведение к обьекту
+  selector: 'app-root',//как его подключить в разметке ,корневой єлемент,селктор определяет тег для подключения компонента
+  standalone: true,//если правда то компонент является автономнім 
+  imports: [RouterOutlet,HeaderComponent,LogoComponent],//какие другие файлі он использует ,все его модели что он будет использовать 
+  templateUrl: './app.component.html',//разметка штмл
+  styleUrl: './app.component.css'//стилизация
 })
 export class AppComponent {
   title = 'First';
 }
+
+//По сути компонент єто кусок разметки со своей стелизацией и так далее.
